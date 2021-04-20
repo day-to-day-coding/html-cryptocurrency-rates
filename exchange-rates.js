b9c5919ed46311f5e88d05c48791e93d88e1b762
schadokar <shubhamchadokar04@gmail.com>
2021-04-20 13:30:05 +0530

    pagination in progress

--- exchange-rates.js
+++ exchange-rates.js
@@ -1,4 +1,4 @@
-function getExchangeRates(event, baseCurrency = "BTC") {
+function getExchangeRates(event, baseCurrency = "BTC", page = 1, rows = 10) {
   var requestOptions = {
     method: "GET",
     redirect: "follow",
@@ -10,21 +10,21 @@ function getExchangeRates(event, baseCurrency = "BTC") {
   )
     .then((response) => response.json())
     .then((result) => {
+      // renderPagination
+      renderPagination(page);
       // render currency table
-      renderCurrencyTable(result.data.rates, baseCurrency);
+      renderCurrencyTable(result.data.rates, baseCurrency, page, rows);
     })
     .catch((error) => console.log("error", error));
 }
 
-function renderCurrencyTable(rates, baseCurrency) {
+function renderCurrencyTable(rates, baseCurrency, page, rows) {
   // get the tbody element
   const tbodyElement = document.getElementById("ratetable");
   tbodyElement.textContent = "";
-  // list of currency
-  const currencyList = ["BTC", "ETH", "LTC", "INR", "USD", "XLM", "BSV", "EUR"];
 
-  for (let i = 0; i < currencyList.length; i++) {
-    const currency = currencyList[i];
+  for (let i = (page - 1) * rows; i < page * rows; i++) {
+    const currency = Object.keys(rates)[i];
     // create a new table row element
     const tr = document.createElement("tr");
     tr.id = currency;
@@ -38,4 +38,21 @@ function renderCurrencyTable(rates, baseCurrency) {
     tr.appendChild(rateTd);
     tbodyElement.appendChild(tr);
   }
+}
+
+function renderPagination(currentPage = 1, nextPage = 1) {
+  const uiElement = document.getElementById("rate-pagination");
+  uiElement.textContent = "";
+  const leftLiElement = document.createElement("li");
+  leftLiElement.id = "left-page";
+  leftLiElement.className = "disabled";
+  const leftLiAnchorElement = document.createElement("a");
+  leftLiAnchorElement.onclick = `getExchangeRates("BTC", 1,5)`;
+  const leftLiIconElement = document.createElement("i");
+  leftLiIconElement.className = "material-icons";
+  leftLiIconElement.textContent = "chevron_left";
+
+  leftLiAnchorElement.appendChild(leftLiIconElement);
+  leftLiElement.appendChild(leftLiAnchorElement);
+  uiElement.appendChild(leftLiElement);
 }
